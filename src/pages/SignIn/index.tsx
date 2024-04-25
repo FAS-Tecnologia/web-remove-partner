39c1375f4ea276143a1ed82fbdf2c13940ebb9dd
Lubni Morais <lubni.morais@gmail.com>
2024-04-25 11:55:50 -0300

    chore: add title platform

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,72 +1,83 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
-import { useForm } from "react-hook-form"
+import { useForm } from 'react-hook-form';
 
-import {z as zod} from "zod"
+import { z as zod } from 'zod';
 
-import { zodResolver} from '@hookform/resolvers/zod'
+import { zodResolver } from '@hookform/resolvers/zod';
 
-import { useMutation } from "@tanstack/react-query"
+import { useMutation } from '@tanstack/react-query';
 
-import { AxiosError } from "axios"
+import { AxiosError } from 'axios';
 
-import { api } from "../../lib/api"
+import { api } from '../../lib/api';
 
-import { SignInButton, SignInContainer, SignInContent, SignInForm, SignInInputContainer, SignInTitle, Title } from "./styles"
+import {
+  SignInButton,
+  SignInContainer,
+  SignInContent,
+  SignInForm,
+  SignInInputContainer,
+  SignInTitle,
+  Title,
+} from './styles';
 
 type IPartner = {
   id: string;
-}
+};
 
 type ISessionResponse = {
-  partner: IPartner
-}
+  partner: IPartner;
+};
 
 const signInValidationSchema = zod.object({
   email: zod.string().email().min(1),
-  password: zod.string().min(1)
-})
+  password: zod.string().min(1),
+});
 
-type ISignInData = zod.infer<typeof signInValidationSchema>
+type ISignInData = zod.infer<typeof signInValidationSchema>;
 
 export const SignInPage = () => {
   const navigate = useNavigate();
 
-  const { register, handleSubmit, formState: { errors}} = useForm<ISignInData>({
-    resolver: zodResolver(signInValidationSchema)
-  })
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ISignInData>({
+    resolver: zodResolver(signInValidationSchema),
+  });
 
-  const { mutateAsync: handleSignIn, isPending: isLoadingSingIn } = useMutation({
-    mutationFn: async (data: ISignInData) => {
-      const response = await api.post('/sessions/partner', data);
+  const { mutateAsync: handleSignIn, isPending: isLoadingSingIn } = useMutation(
+    {
+      mutationFn: async (data: ISignInData) => {
+        const response = await api.post('/sessions/partner', data);
 
-      return response.data as ISessionResponse;
-    },
-    onSuccess: async (partnerData) => {
-      const partnerId = partnerData.partner.id
+        return response.data as ISessionResponse;
+      },
+      onSuccess: async (partnerData) => {
+        const partnerId = partnerData.partner.id;
 
-      navigate(`/remover?partnerId=${partnerId}`)
-    },
-    onError: (error) => {
-      if (error instanceof AxiosError) {
-        if (error.response) {
-          console.log(error.response.data)
+        navigate(`/remover?partnerId=${partnerId}`);
+      },
+      onError: (error) => {
+        if (error instanceof AxiosError) {
+          if (error.response) {
+            console.log(error.response.data);
+          }
         }
-      }
-    }
-  })
+      },
+    },
+  );
 
   const handleSubmitSignIn = (data: ISignInData) => {
-    handleSignIn(data)
-  }
-  
+    handleSignIn(data);
+  };
+
   return (
     <SignInContainer>
-      
       <SignInContent>
-      <Title>
-          Parceiros - FAS Tecnologia
-        </Title>
+        <Title>Parceiros - FAS Tecnologia</Title>
 
         <SignInForm onSubmit={handleSubmit(handleSubmitSignIn)}>
           <SignInTitle>Faça o login</SignInTitle>
@@ -76,12 +87,18 @@ export const SignInPage = () => {
           </SignInInputContainer>
 
           <SignInInputContainer isError={!!errors.password}>
-            <input type="password" placeholder="Senha" {...register('password')} />
+            <input
+              type="password"
+              placeholder="Senha"
+              {...register('password')}
+            />
           </SignInInputContainer>
 
-          <SignInButton type="submit" disabled={isLoadingSingIn}>Entrar</SignInButton>
+          <SignInButton type="submit" disabled={isLoadingSingIn}>
+            Entrar
+          </SignInButton>
         </SignInForm>
       </SignInContent>
     </SignInContainer>
-  )
-}
+  );
+};
